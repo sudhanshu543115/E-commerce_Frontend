f2ab618d445b2d77e65fee119e292b35254eb5f1
SUDHANSHU DUBEY <manudubey249@gmail.com>
2025-08-22 20:39:56 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Routes , Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import CustomerRouters from "./Routers/CustomerRouters.jsx";
+
+function App() {
+  return (
+    <AuthProvider>
+      <div>
+        <Routes>
+          <Route path="/*" element={<CustomerRouters />} />
+        </Routes>
+      </div>
+    </AuthProvider>
+  );
+}
+
+export default App;
